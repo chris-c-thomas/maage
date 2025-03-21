b23be052bcacab9a5ca044eeea08a13f02c2e0ad
Chris Thomas <ccthomas@anl.gov>
2025-03-21 10:27:42 -0500

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./views/**/*.ejs",
+    "./public/js/**/*.js",
+    "!./public/js/**/node_modules/**/*",
+    "!./public/js/**/.git/**/*",
+    "./public/**/*.html",
+    "!./public/**/node_modules/**/*",
+    "!./public/**/.git/**/*",
+    "./routes/**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
